c35a38fac79d307c08d8290116a62153fcfe0bdc
MinchinWeb <w_minchin@hotmail.com>
2023-11-12 19:43:56 -0700

    [Tasks] add ability to feed GitHub API to task

--- tasks.py
+++ tasks.py
@@ -13,20 +13,25 @@ CKAN_DIR = CKAN_DIR.resolve()
 
 
 @task
-def make_ckan(ctx):
+def make_ckan(ctx, github_token=None):
     print("**starting**")
     # print(HERE)
     print(f"{NETKAN_DIR=!s}")
     print(f"{CKAN_DIR=!s}")
+
+    if github_token:
+        GITHUB_TOKEN_STR = f" --github-token {github_token}"
+
     for dirpath, dirnames, filenames in NETKAN_DIR.walk():
         # print(dirpath, dirnames, filenames)
         for fn in filenames:
             fn = Path(fn)
+            print()
             print(f"    {fn!s}")
             output_dir = CKAN_DIR / fn.stem
             output_dir.mkdir(exist_ok=True)
 
-            ctx.run(f"{NETKAN_EXE} --outputdir {output_dir} --verbose {NETKAN_DIR / fn}")
+            ctx.run(f"{NETKAN_EXE} --outputdir {output_dir} --verbose{GITHUB_TOKEN_STR} {NETKAN_DIR / fn}")
     print("**end**")
     print("")
     print("Make sure to push an update to the metadata repo!")
@@ -39,6 +44,7 @@ def apply_patches(ctx):
         for fn in filenames:
             fn = Path(fn)
             if fn.suffix == ".patch":
+                print()
                 print(f"    {fn!s}")
                 
                 ctx.run(f"git am {fn}")
